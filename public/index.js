32c776f5f78a175ec70510d56bd3117a0298e4e7
coryrgriffith <corygriffith9@gmail.com>
2018-08-14 21:01:22 -0500

    add and install vue.js and bootstrap, index and show modal for posts, front end user authentication

---
+++ public/index.js
@@ -0,0 +1,167 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      posts: [
+        {
+          user_id: "",
+          title: "",
+          body: "",
+          image: ""
+        }
+      ],
+      selectedPost: []
+    };
+  },
+  created: function() {
+    axios.get('/v1/posts').then(function(response) {
+      this.posts = response.data;
+    }.bind(this));
+  },
+  methods: {
+    changePost: function(inputPost) {
+      this.selectedPost = inputPost;
+    }
+  },
+  computed: {}
+};
+
+var NewPostPage = {
+  template: "#new-post-page",
+  data: function() {
+    return {
+      user_id: "",
+      title: "",
+      body: "",
+      image: ""
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        user_id: this.$route.params.id,
+        title: this.title,
+        body: this.body,
+        image: this.image
+      };
+      console.log('in submit');
+      console.log(params);
+
+      axios
+        .post("/v1/posts", params)
+        .then(function(response) {
+          console.log('in post');
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/api/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: {
+          email: this.email, password: this.password
+        }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LogoutPage = {
+  template: "<h1>Logout</h1>",
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
+var router = new VueRouter({
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage },
+    { path: "/posts/new", component: NewPostPage }
+  ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
+});
